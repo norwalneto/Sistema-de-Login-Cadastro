51a0b68eec8bde5c5ce3904dd726d9eff8fc944c
norwalneto <norwalgomess@gmail.com>
2024-12-17 22:05:15 -0300

    implementação de arquivos inicais.

---
+++ backend/sistema-de-login-cadastro/src/main/java/com/projeto/sistema_de_login_cadastro/SistemaDeLoginCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_de_login_cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeLoginCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeLoginCadastroApplication.class, args);
+	}
+
+}
---
+++ backend/sistema-de-login-cadastro/src/test/java/com/projeto/sistema_de_login_cadastro/SistemaDeLoginCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_de_login_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeLoginCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
