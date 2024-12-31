3003d7f00650a3e70c9e8cb915f8f926c79e9026
norwalneto <norwalgomess@gmail.com>
2024-12-31 02:51:10 -0300

    atualizações

--- backend/src/main/java/com/projeto/sistema_de_login_cadastro/SistemaDeLoginCadastroApplication.java
+++ backend/src/main/java/com/projeto/sistema_de_login_cadastro/SistemaDeLoginCadastroApplication.java
@@ -1,9 +1,12 @@
 package com.projeto.sistema_de_login_cadastro;
 
+import com.azure.spring.cloud.autoconfigure.implementation.context.AzureTokenCredentialAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {AzureTokenCredentialAutoConfiguration.class})
 public class SistemaDeLoginCadastroApplication {
 
 	public static void main(String[] args) {
